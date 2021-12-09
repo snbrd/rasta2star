90dc2cc5089d6bc322908165f926fc5e7a0dbb2c
Yogi Bagas <bagasdikara@gmail.com>
2021-12-09 12:09:48 +0800

    add box-inset shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -215,6 +215,7 @@ module.exports = {
       '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
       outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+      inset: 'inset 0px -5px 4px rgba(255, 230, 0, 0.86)',
       box: '2px 2px 5px 3px rgba(0, 0, 0, 0.1);',
       none: 'none',
     },
