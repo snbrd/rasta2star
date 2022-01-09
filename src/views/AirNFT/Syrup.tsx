d8131a16d17ec8bec11d9aa2159b0460dc16d0f0
✨Starbinna✨ <92035565+starbinna@users.noreply.github.com>
2022-01-09 21:21:59 +0900

    Add countdown page

---
+++ src/views/AirNFT/Syrup.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Fade from 'react-reveal/Fade';
+
+import {
+  MainWrapper,
+  MainContentSection,
+  NormalClockWrapper,
+  Container,
+} from './deadline';
+// import NormalClock from './components/NormalClock/NormalClock';
+import BannerBG from "../../assets/deadline.png";
+
+const Farm: React.FC = () => {
+  // const deadline = new Date(new Date().getTime() + 6 * 60 * 60 * 1000);
+  return (
+    <div>
+      <div>
+        <MainWrapper
+          style={{
+            backgroundImage: `url(${BannerBG})`,
+            backgroundColor: '#e6e8ee',
+            backgroundPosition: 'bottom',
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+            display: 'flex',
+            alignItems: 'center'
+          }}
+        >
+          <Container className="mainContainer">
+            <MainContentSection>
+              <h2>
+                AirNFT Farm is coming soon
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed eiu sit amet consectetur adipisicing
+              </p>
+              <Fade>
+                <NormalClockWrapper>
+                  {/* <NormalClock countdown={deadline} /> */}
+                </NormalClockWrapper>
+              </Fade>
+            </MainContentSection>
+          </Container>
+        </MainWrapper>
+      </div>
+    </div >
+  )
+}
+
+
+export default Farm
