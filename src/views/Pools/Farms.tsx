00d2e41864121b2ba558b7a0a8ad1fac044b8b0f
✨Starbinna✨ <92035565+starbinna@users.noreply.github.com>
2022-01-25 15:28:52 +0900

    Fix token price and Apr issue

--- src/views/Pools/Farms.tsx
+++ src/views/Pools/Farms.tsx
@@ -64,8 +64,8 @@ const Farms: React.FC = () => {
   const stackedOnly = false;
 
   const farmsLP = farmList.filter((farm) => !farm.farm)
-  const activeFarms = farmsLP.filter((farm) => farm.multiplier !== '0X')
-  const inactiveFarms = farmsLP.filter((farm) => farm.multiplier === '0X')
+  const activeFarms = farmsLP.filter((farm) => farm.pid !== 0 && farm.multiplier !== '0X')
+  const inactiveFarms = farmsLP.filter((farm) => farm.pid !== 0 && farm.multiplier === '0X')
   const stackedOnlyFarms = activeFarms.filter(
     (farm) => farm.userData && new BigNumber(farm.userData.stakedBalance).isGreaterThan(0),
   )
