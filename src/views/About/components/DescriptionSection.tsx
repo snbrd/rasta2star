cd7b1a24337df850ec2f281134636d8160399b2a
sndbrd <snbrd@protonmail.com>
2022-02-02 02:01:02 -0500

    try

--- src/views/About/components/DescriptionSection.tsx
+++ src/views/About/components/DescriptionSection.tsx
@@ -5,13 +5,13 @@ export default function DescriptionSection() {
     <div className="max-w-screen-xl mx-auto text-center text-black space-y-6">
       <p>
         RastaFinance is a truly decentralized initiative building simple tools for creators to conduct and manage their own finances.
-       </p>
-  //    <p>
-  //      We value transparency, love, compassion and camaraderie above all else. We stand strong as ONE, as we are
-  //      determined to re-gain the power of our own money, taking it back from the institutions and exploiters that have
-//        kept us apart.
-//      </p>
-//      <p>So connect your browser-based wallet, and join the Decentralized REVOLUTION!</p>
+      </p>
+      <p>
+        We value transparency, love, compassion and camaraderie above all else. We stand strong as ONE, as we are
+        determined to re-gain the power of our own money, taking it back from the institutions and exploiters that have
+        kept us apart.
+      </p>
+      <p>So connect your browser-based wallet, and join the Decentralized REVOLUTION!</p>
     </div>
   )
 }
