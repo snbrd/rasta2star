e42bb37f3f8428678a0440400d364e00ce1b0249
✨Atosinu✨ <atosinu@gmail.com>
2022-03-18 14:15:32 +0900

    init AOS

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import AOS from 'aos';
 import App from './App'
 import Providers from './Providers'
+import 'aos/dist/aos.css';
 import './index.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+
+AOS.init();
 
 ReactDOM.render(
   <React.StrictMode>
