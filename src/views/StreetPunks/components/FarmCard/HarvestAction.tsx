3162fdcca54c3a93f9020578693f8992d96d37d6
Atosinu <atosinu@gmail.com>
2022-04-21 14:59:58 +0900

    Update HarvestAction.tsx

--- src/views/StreetPunks/components/FarmCard/HarvestAction.tsx
+++ src/views/StreetPunks/components/FarmCard/HarvestAction.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BigNumber from 'bignumber.js'
 import * as FaIcons from 'react-icons/fa'
 import { getBalanceNumber } from 'utils/formatBalance'
@@ -10,24 +10,28 @@ interface FarmCardActionsProps {
   poolAddress: any
 }
 
-const HarvestAction: React.FC<FarmCardActionsProps> = ({ earnings, poolAddress }) => {
-  // const [pendingTx, setPendingTx] = useState(false)
+const HarvestAction: React.FC<FarmCardActionsProps> = ({ earnings, type, poolAddress }) => {
+  const [pendingTx, setPendingTx] = useState(false)
   const rawEarningsBalance = getBalanceNumber(earnings)
-  const buttonClass = "disabled px-4 py-2 flex-row space-x-2 flex w-full items-center justify-center cursor-pointer"
+  const buttonClass = "px-4 py-2 flex-row space-x-2 flex w-full items-center justify-center cursor-pointer"
   const { onClaim } = useClaim(poolAddress)
+  
   return (
     <div className="harvest flex mt-4 bg-gradient-to-l text-white w-full from-green-rasta to-yellow-rasta  rounded-md">
       <button
         type="button"
-        disabled
-        // disabled={rawEarningsBalance === 0 || pendingTx || !type}
-        // className={(rawEarningsBalance === 0 || !type ? "disabled " : "") + buttonClass}
-        className={buttonClass}
+        disabled={rawEarningsBalance === 0 || pendingTx || !type}
+        className={(rawEarningsBalance === 0 || !type ? "disabled " : "") + buttonClass}
         onClick={async () => {
           if (rawEarningsBalance > 0) {
-            // setPendingTx(true)
-            await onClaim();
-            // setPendingTx(false)
+            setPendingTx(true)
+            try {
+              await onClaim();
+              setPendingTx(false)
+            } catch (error) {
+              console.log(error)
+              setPendingTx(false)
+            }
           }
         }}
       >
