4eade6a26cdde90e3d99e5d7034fed8a5ef66cb8
Putra Dinata <putradnata15@gmail.com>
2023-02-02 15:36:30 +0800

    update: new content about page

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -24,11 +24,19 @@ return(
                     <LazyLoadImage src={ArtistsImage} alt="" effect="blur" className="about-image" />
                 </div>
                 <div className='flex flex-col items-center md:items-start justify-center px-5 text-center md:text-left md:px-0 gap-y-4'>
-                    <span>lipsum</span>
-                    <h2 className='text-4xl text-heading font-bold'>LIPSUM DOLOR</h2>
+                    <span>About the</span>
+                    <h2 className='text-4xl text-heading font-bold'>Zion launch Pad</h2>
 
                     <p>
-                        lipsum dolor sit amet aajsdjasdjladjsladjslajdlkjasdlkjasdklj
+                    The Zion Launchpad is an NFT based utility minting / launchpad for artists. 
+                    </p>
+
+                    <p>
+                    Artists that come to the launchpad are able to earn revenue from NFT sales which the user can further more
+                    </p>
+
+                    <p>
+                    stake their purchased NFT from the music artists on the Zion Labs platform.
                     </p>
                 </div>
             </div>
@@ -36,8 +44,18 @@ return(
         <div className='h-screen' style={{backgroundImage: `url(${ThirdSection})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
             <div className='h-full w-full'>
                 <div className='h-full w-full flex items-center justify-center flex-col'>
-                    <span>Lipsum dolor</span>
-                    <h1 className='text-4xl md:text-6xl font-bold text-heading'>Lorem ipsum</h1>
+                    <span>From us</span>
+                    <h1 className='text-4xl md:text-6xl font-bold text-heading'>For You</h1>
+
+                    <p className='break-words'>
+                        Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet 
+                    </p>
+                    <p className='break-words'>
+                        Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                    </p>
+                    <p className='break-words'>
+                        Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet 
+                    </p>
                 </div>
             </div>
         </div>
