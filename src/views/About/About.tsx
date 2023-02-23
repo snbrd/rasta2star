b36e641ef68a4704f0c1b9f59ba061016052592a
Putra Dinata <putradnata15@gmail.com>
2023-02-23 17:55:39 +0800

    revision: badger 3

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -28,19 +28,29 @@ return(
                     <span>About the</span>
                     <h2 className='text-4xl text-heading font-bold'>Zion launch Pad</h2>
                     
-                    <div className='flex flex-col gap-4 mt-4'>
+                    <div className='flex flex-col gap-4 mt-4 md:pr-16'>
                     <p>
-                    The Zion Launchpad is an NFT based utility minting / launchpad for artists. 
+                    {/* The Zion Launchpad is an NFT based utility minting / launchpad for artists.  */}
+                    Welcome to Zion Labs, the ultimate NFT-based utility minting and launchpad platform for artists!
                     </p>
 
                     <p>
-                    Artists that come to the launchpad are able to earn revenue from NFT sales which the user can further more
+                    {/* Artists that come to the launchpad are able to earn revenue from NFT sales which the user can further more */}
+                    Our platform provides a space for artists to showcase their work and earn revenue from NFT sales. With our innovative launchpad, artists can easily mint and sell their NFTs to a global audience of art collectors and enthusiasts.
                     </p>
 
                     <p>
-                    stake their purchased NFT from the music artists on the Zion Labs platform.
+                    {/* stake their purchased NFT from the music artists on the Zion Labs platform. */}
+                    Not only can artists earn revenue from their NFT sales, but users can also further monetize their purchases by staking them on our platform. By staking NFTs from music artists on Zion Labs, users can earn even more revenue and contribute to the growth of the art community.
                     </p>
+
+                    <p>
+                    Join our community of artists and art enthusiasts today and experience the power of Zion Labs for yourself!
+                    </p>
+
                     </div>
+
+                    <a className='p-5 border-white border-1 mt-5 hover:bg-white hover:text-black' href="https://www.zionlp.com/launchpad-artists">Go to Launchpad</a>
                 </div>
             </div>
         </div>
@@ -50,15 +60,20 @@ return(
                     <span>From us</span>
                     <h1 className='text-4xl md:text-6xl font-bold text-heading'>For You</h1>
 
-                    <p className='break-words text-center'>
-                        Since our conception in 2020, we have been studying and discovering novel ways of revolutionizing the music industry, 
-                    </p>
-                    <p className='break-words text-center'>
-                        We want to help empower artists, to expand their horizons and reach an audience without borders.
-                    </p>
-                    <p className='break-words text-center'>
-                        Using novel Web3 protocols, we seek to bring transparent innovation to the masses.
-                    </p>
+                    <div className='flex flex-col gap-y-8 md:px-32'>
+                        <p className='break-words text-center'>
+                            {/* Since our conception in 2020, we have been studying and discovering novel ways of revolutionizing the music industry,  */}
+                            Our platform utilizes innovative Web3 protocols to bring transparent and decentralized solutions to the masses. We believe that by leveraging blockchain technology, we can help create a more equitable and accessible music industry that benefits artists and fans alike.
+                        </p>
+                        <p className='break-words text-center'>
+                            {/* We want to help empower artists, to expand their horizons and reach an audience without borders. */}
+                            Our mission is to empower artists to take control of their careers and connect with fans on a global scale. We provide a range of services to help artists succeed, including NFT minting, launchpad, and staking features.
+                        </p>
+                        <p className='break-words text-center'>
+                            {/* Using novel Web3 protocols, we seek to bring transparent innovation to the masses. */}
+                            Join us on our journey to revolutionize the music industry and create a more transparent and equitable future for artists!
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -38,11 +38,41 @@ const Home: React.FC = () => {
           <CarouselSlider/>
           {/* <Mint /> */}
           {/* <Community /> */}
+
+          <div className="flex w-full bg-white text-black flex-col py-12 md:py-16">
+            <div className="container mx-auto">
+                <h2 data-aos="fade-up" data-aos-duration="1000" className="text-2xl text-center md:text-5xl font-bold leading-tight">
+                ZION LION NFTs
+                </h2>
+
+                <div className='flex flex-col gap-y-5 mt-10 lg:px-24'>
+                  <p className='text-center'>
+                  Welcome to Zion Labs&apos; Novel Collection of Zion Lions - the ultimate NFT collection for collectors and enthusiasts looking for unique and valuable digital assets.
+                  </p>
+                  <p className='text-center'>
+                  The Zion Lions are a one-of-a-kind collection of 6200 randomly minted NFTs, each with its own rarity class and corresponding rewards. As a collector, you have the opportunity to acquire one or more of these rare and valuable digital assets and stake them on our platform to earn high APR rewards.
+                  </p>
+                  <p className='text-center'>
+                  What sets the Zion Lions apart from other NFT collections is their set BNB value, which provides a transparent and reliable way to evaluate their worth. Whether you&apos;re a seasoned collector or just getting started, the Zion Lions offer a unique and exciting opportunity to participate in the rapidly growing world of NFTs.
+                  </p>
+                  <p className='text-center'>
+                  To acquire your own Zion Lion, simply visit our minting page and purchase a randomly generated NFT from our collection. With affordable minting fees and the potential for high rewards, the Zion Lions are the perfect addition to any NFT collection.
+                  </p>
+                  <p className='text-center'>
+                  Join our community of collectors and enthusiasts today and start minting your Zion Lions on Zion Labs&apos; platform!
+                  </p>
+
+                </div>
+            </div>
+          </div>
+
           <div className="flex w-full bg-white text-black flex-col py-12 md:py-16">
             <div className="container mx-auto">
               <div className="mx-auto mb-8">
               <h2 data-aos="fade-up" data-aos-duration="1000" className="text-2xl text-center md:text-5xl font-bold leading-tight">
-             NFTNOMICS
+             {/* NFTNOMICS */}
+             
+             ZION LION NFTNOMICS
             </h2>
                 {/* <LazyLoadImage
                   src={NFTNomicsImage}
@@ -56,16 +86,16 @@ const Home: React.FC = () => {
 
               <div className="flex flex-col md:flex-row">
                 <div
-                  className="items-center flex justify-center flex-1 pb-4 px-5 md:px-0 md:pb-0"
+                  className="items-center flex justify-center flex-1 pb-4 px-5 md:px-16 md:pb-0"
                   data-aos="fade-right"
                   data-aos-duration="1000"
                 >
-                  <LazyLoadImage src={ZIONLIONImage} alt="t4-professor" className="w-auto" effect="blur" />
+                  <LazyLoadImage src={ZIONLIONImage} alt="t4-professor" className="w-full" effect="blur" />
                 </div>
 
                 <div className="flex flex-col px-3 md:px-12 space-y-6">
                   <div
-                    className="h-full shadow-xl p-8 rounded-lg flex flex-col justify-between space-y-5"
+                    className="h-full shadow-xl p-8 rounded-lg flex flex-col justify-center space-y-5"
                     data-aos="fade-left"
                     data-aos-duration="1000"
                     data-aos-delay="100"
@@ -100,7 +130,7 @@ const Home: React.FC = () => {
                   </div>
 
                   <div
-                    className="h-full shadow-xl p-8 rounded-lgg flex flex-col justify-between space-y-5"
+                    className="h-full shadow-xl p-8 rounded-lgg flex flex-col justify-center space-y-5"
                     data-aos="fade-left"
                     data-aos-duration="1000"
                     data-aos-delay="200"
@@ -108,7 +138,8 @@ const Home: React.FC = () => {
                     <div className="card-title flex justify-between">
                       <h2 className="md:text-xl font-bold pr-0 md:pr-48 text-left md:text-left leading-tight flex-1">
                         {/* Launchpad NFTs - MRASTA */}
-                        STAKING TOKENOMICS
+                        {/* STAKING TOKENOMICS */}
+                        0.18BNB Stake Weight
                       </h2>
 
                       {/* <a
@@ -124,16 +155,20 @@ const Home: React.FC = () => {
                     <div className="card-body">
                       <ul className="space-y-2">
                         <li>
-                          Harvest is locked for 30 days
+                          {/* Harvest is locked for 30 days */}
+                          Farmer Upto 15% APR
                         </li>
                         <li>
-                          User receives 70% of Harvest (2 years)
+                          {/* User receives 70% of Harvest (2 years) */}
+                          Explorer Upto 122%APY
                         </li>
                         <li>
-                          20% for Marketing
+                          {/* 20% for Marketing */}
+                          Builder Upto 128% APR
                         </li>
                         <li>
-                          10% of all Harvest rewards are put into liquidity for MRASTA/RASTA LP
+                          {/* 10% of all Harvest rewards are put into liquidity for MRASTA/RASTA LP */}
+                          Professor - Mega APR
                         </li>
                         {/* <li>25% to Artist</li>
                         <li>25% to Creator</li>
@@ -148,17 +183,17 @@ const Home: React.FC = () => {
                     </div> */}
                   </div>
 
-                  <div
+                  {/* <div
                     className="h-full shadow-xl p-8 rounded-lg flex flex-col justify-between space-y-5"
                     data-aos="fade-left"
                     data-aos-duration="1000"
                     data-aos-delay="300"
                   >
                     <div className="card-title flex justify-between items-center">
-                      <h2 className="md:text-xl font-bold pr-0 md:pr-48 text-left md:text-left leading-tight flex-1">
+                      <h2 className="md:text-xl font-bold pr-0 md:pr-48 text-left md:text-left leading-tight flex-1"> */}
                         {/* Launchpad Staking Tokenomics */}
-                        SALE TOKENOMICS
-                      </h2>
+                        {/* SALE TOKENOMICS
+                      </h2> */}
 
                       {/* <a
                         className="bg-gradient-to-b font-bold text-white from-blue-zion to-blue-zion_cyan text-center py-2 px-8 rounded-xl"
@@ -168,7 +203,7 @@ const Home: React.FC = () => {
                       >
                         Read More
                       </a> */}
-                    </div>
+                    {/* </div>
 
                     <div className="card-body">
                       <ul className="space-y-2">
@@ -189,18 +224,18 @@ const Home: React.FC = () => {
                         </li>
                         <li>
                           5% to Rasta farms
-                        </li>
+                        </li> */}
                         {/* <li>Harvest is locked for 30 days (for both artists and users)</li>
                         <li>User receives 65% of Harvest (2 years)</li>
                         <li>Artist receives 25% of Harvest (90 days)</li>
                         <li>10% of all Harvest rewards are put into liquidity for MRASTA/RASTA LP</li> */}
-                      </ul>
-                    </div>
+                      {/* </ul>
+                    </div> */}
 
                     {/* <div className="card-footer text-center md:text-left">
                       
                     </div> */}
-                  </div>
+                  {/* </div> */}
                 </div>
               </div>
             </div>
