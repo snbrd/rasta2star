b85bee82267f603c085c9ad9cd5a668a149db2db
Putra Dinata <putradnata15@gmail.com>
2023-03-16 15:19:39 +0800

    update: ActivateSwitch

--- src/views/Home/components/Data.tsx
+++ src/views/Home/components/Data.tsx
@@ -1,17 +1,18 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable import/prefer-default-export */
 import Silhouette from '../../../assets/newimage/silhouette.jpg'
-import Lion from '../../../assets/newimage/front-lion.jpg'
+import Lion4388 from '../../../assets/newimage/lion-4388.jpg'
+import Lion4464 from '../../../assets/newimage/lion-4464.jpg'
 
 export const CardsData = [
     {
-        front: <img src={Lion} alt="Lion" className='rounded-md'/>,
-        back: <img src={Silhouette} alt="silhouette" className='rounded-md'/>,
+        front: <img src={Lion4388} alt="Lion" className='rounded-md'/>,
+        back: <img src={Lion4388} alt="Lion" className='rounded-md'/>,
         win: true,
     },{
-        front: '-',
-        back: <img src={Silhouette} alt="silhouette" className='rounded-md'/>,
-        win: false,
+        front: <img src={Lion4464} alt="Lion" className='rounded-md'/>,
+        back: <img src={Lion4464} alt="Lion" className='rounded-md'/>,
+        win: true,
     },
     {
         front: '-',
