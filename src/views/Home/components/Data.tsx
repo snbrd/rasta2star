c0fe9a2169a9bc67809b9dc35aeb1f6f6bcc4798
Putra Dinata <putradnata15@gmail.com>
2023-04-17 09:58:40 +0800

    update: ActivateSwitch

--- src/views/Home/components/Data.tsx
+++ src/views/Home/components/Data.tsx
@@ -6,6 +6,7 @@ import Lion4464 from '../../../assets/newimage/lion-4464.jpg'
 import Lion4545 from '../../../assets/newimage/lion-4545.jpg'
 import Lion4528 from '../../../assets/newimage/lion-4528.jpg'
 import Lion4627 from '../../../assets/newimage/lion-4627.jpg'
+import Lion4683 from '../../../assets/newimage/lion-4683.jpg'
 
 export const CardsData = [
     {
@@ -33,9 +34,9 @@ export const CardsData = [
         win: true,
     },
     {
-        front: '-',
-        back: <img src={Silhouette} alt="silhouette" className='rounded-md'/>,
-        win: false,
+        front: <img src={Lion4683} alt="Lion" className='rounded-md'/>,
+        back: <img src={Lion4683} alt="Lion" className='rounded-md'/>,
+        win: true,
     },
     {
         front: '-',
